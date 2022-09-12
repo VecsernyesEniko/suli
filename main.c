c756240a85c8787b63fe6d997d2113a2ca753279
VecsernyesEniko <77058371+VecsernyesEniko@users.noreply.github.com>
2022-09-12 10:32:07 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+
+int main(){
+    int n=42;
+    unsigned int un=10;
+    float f=3.14;
+    double d=3.141592;
+    char c='a';
+
+    printf("%d %u %f %lf %c \n", n,un,f,d,c);
+}
